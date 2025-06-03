a9d30bc9676981b3714aad27e6d7bc051ec81ede
Charlie Karoubi <86491427+CharlieKaroubi@users.noreply.github.com>
2025-06-03 11:37:02 -0700

    Added navbar

--- portfolio-app/src/app/page.tsx
+++ portfolio-app/src/app/page.tsx
@@ -5,9 +5,28 @@ import StaggeredHover from "@/components/StaggeredHover";
 export default function Home() {
   return (
     <StarfieldBackground>
+    <nav className="sticky top-0 bg-black p-2 z-50">
+      <ul className="flex justify-center space-x-12 text-white font-semibold text-[2.7vw] sm:text-[2.5vw] md:text-[1.3vw]">
+        <li>
+          <a href="#home" className="hover:text-blue-500 transition-colors duration-300">Home</a>
+        </li>
+        <li>
+          <a href="#about" className="hover:text-blue-500 transition-colors duration-300">About</a>
+        </li>
+        <li>
+          <a href="#experience" className="hover:text-blue-500 transition-colors duration-300">Experience</a>
+        </li>
+        <li>
+          <a href="#projects" className="hover:text-blue-500 transition-colors duration-300">Projects</a>
+        </li>
+        <li>
+          <a href="#contact" className="hover:text-blue-500 transition-colors duration-300">Contact</a>
+        </li>
+      </ul>
+    </nav>
     <main className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth">
       <section id="home" className="snap-start flex flex-col items-center justify-center min-h-screen text-center text-[8vw] sm:text-[8vw] md:text-[5vw]">
-        <h1>Hello, I'm <StaggeredHover className="text-blue-500 font-extrabold">Charlie</StaggeredHover></h1>
+        <h1>Hello, I'm <StaggeredHover className="text-blue-500 font-extrabold">Charlie Karoubi</StaggeredHover></h1>
         <h1>I'm an aspiring <StaggeredHover className="font-extrabold text-blue-500">Software Engineer</StaggeredHover></h1>
       </section>
       <section id="about" className="snap-start flex flex-col items-center justify-center min-h-screen text-center">
@@ -17,6 +36,12 @@ export default function Home() {
           secure, and impactful backend systems. I'm always exploring new technologies and methods to optimize 
           both my code and personal productivity. Outside of tech, I'm a big sports fan (especially France and PSG), 
           and I love staying active—whether that's at the gym, cooking up new recipes, or spending time with friends, family, and my cat, Kiki.
+        </p>
+      </section>
+      <section id="experience" className="snap-start flex flex-col items-center justify-center min-h-screen text-center">
+        <StaggeredHover className="text-[10vw] sm:text-[8vw] md:text-[5vw] text-blue-500 font-extrabold">Experience</StaggeredHover>
+        <p className="text-[5vw] sm:text-[4.5vw] md:text-[3vw]">
+          
         </p>
       </section>
     </main>
